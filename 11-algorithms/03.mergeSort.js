a5cab4e42d6cfb6b255e1163d9875eeb74f5c2b2
karalkal <karalkal@yahoo.co.uk>
2024-03-19 19:08:46 +0000

    added quickSort exercise

--- 11-algorithms/03.mergeSort.js
+++ 11-algorithms/03.mergeSort.js
@@ -10,6 +10,7 @@ const mergeSort = (startArray) => {
     const mid = Math.floor(length / 2);
     const leftArray = startArray.slice(0, mid);
     const rightArray = startArray.slice(mid, length);
+    console.log("left:", leftArray, "<---->", "right:", rightArray);
 
     return merge(mergeSort(leftArray), mergeSort(rightArray))
 }
@@ -18,6 +19,7 @@ const merge = (leftArray, rightArray) => {
     const sortedArray = [];
     while (leftArray && rightArray && leftArray.length > 0 && rightArray.length > 0) {
         let numberToAppend
+        console.log("comparing", leftArray, "AND", rightArray);
         if (leftArray[0] < rightArray[0]) {
             numberToAppend = leftArray.shift();
         }
@@ -27,13 +29,20 @@ const merge = (leftArray, rightArray) => {
         sortedArray.push(numberToAppend);
         console.log("current sorted array state:", sortedArray);
     }
-    // while loop continues until either leftArray or rightArray is empty, you need to concatenate whatever is left in the other array to the sorted array
+    // while loop continues until either leftArray or rightArray is empty, 
+    // you need to concatenate whatever is left in the other array to the sorted array
+    // in this case must be the largest number
     return sortedArray.concat(leftArray).concat(rightArray);
 }
 
 
 const inputArr = [3, 5, 2, 90, 4, 7];
-
+console.log("\nORIGINAL:", inputArr)
 console.log(mergeSort(inputArr));
+
+const inputArr1 = [88, 3, 5, 2, 88, 4, 7, 88, 1];
+console.log(mergeSort(inputArr1));
+
+
 
 
