8407eb673ee1cd0b4b467d34b62650f8007e1201
karalkal <karalkal@yahoo.co.uk>
2024-03-20 03:25:10 +0000

    added graph traversal (upto step 2 in last stage)

---
+++ 11-algorithms/06.graphTraversal/breadthFirstTraversal.js
@@ -0,0 +1,25 @@
+const testGraph = require("./testGraph.js");
+const Queue = require('./Queue.js');
+
+/*
+As we iterate through the neighbors, we will add its connected vertices to the end of the queue, pull off the next neighbor from the queue, add its connected vertices, and so on. This way allows us to maintain the visiting order; we will visit the vertices across the same layer while queueing up the next layer. When there are no vertices left in the current layer, the vertices of the next layer are already queued up, so we move down and iterate across the next layer.
+*/
+
+const breadthFirstTraversal = (start) => {
+    const visitedVertices = [start];
+    const visitQueue = new Queue();
+    visitQueue.enqueue(start);
+    const current = visitQueue.dequeue();
+    console.log(current.data)
+
+    start.edges.forEach((edge) => {
+        const neighbor = edge.end;
+
+        if (!visitedVertices.includes(neighbor)) {
+            visitedVertices.push(neighbor);
+        }
+    });
+};
+
+breadthFirstTraversal(testGraph.vertices[0]);
+
---
+++ 11-algorithms/06.graphTraversal/depthFirstTraversal.js
@@ -0,0 +1,17 @@
+const testGraph = require('./testGraph.js');
+
+// pass vertice where traversal must start
+const depthFirstTraversal = (start, callback, visitedVertices = [start]) => {
+    callback(start);
+
+    start.edges.forEach((edge) => {
+        const neighbor = edge.end;
+
+        if (!visitedVertices.includes(neighbor)) {
+            visitedVertices.push(neighbor);
+            depthFirstTraversal(neighbor, callback, visitedVertices);
+        }
+    });
+};
+
+depthFirstTraversal(testGraph.vertices[0], (vertex) => { console.log(vertex.data) });
